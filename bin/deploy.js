aebdb90d28787b775906f642343a70b091689ab0
azl397985856 <695694307@qq.com>
2017-02-27 11:12:29 +0800

    print the effected files

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,6 +2,8 @@
 
 var program = require('commander');
 var fs = require('fs');
+var map = require('map-stream');
+var vfs = require( 'vinyl-fs' );
 var ftp = require( 'vinyl-ftp' );
 
 program
@@ -11,14 +13,22 @@ program
   .parse(process.argv);
 
 var deploy_env = program.args[0];
-var config = program.config ? program.config : 'deploy.json'
+var config = program.config ? program.config : 'deploy.json';
+var log = function(file, cb) {
+  console.log(file.path);
+  cb(null, file);
+};
 fs.readFile(config, function(err,data) {
     if (!err) {
         // logic here
         var config = JSON.parse(data.toString('utf-8'))[deploy_env]
         if (config) {   
-            var conn = ftp.create(config);
-            conn.dest(config.dest);
+            var conn = new ftp(config);
+            vfs.src(config.workspace, { buffer: false } )
+                .pipe(map(log)) // print the effected files
+                .pipe( conn.dest(config.dest, {
+                    sourcemaps: config.sourcemaps,
+                }))
             console.log('deploy success!');
         } else {
             console.error('deploy_env not found');
@@ -27,3 +37,7 @@ fs.readFile(config, function(err,data) {
         console.error('config file not found');
     }
 });
+// print uncaughtException for debug
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
