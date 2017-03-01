45801a610b6eb97b838a326f75ccbd425a2fcfeb
azl397985856 <695694307@qq.com>
2017-03-01 14:36:27 +0800

    change to be sftp based

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,14 +2,12 @@
 
 var program = require('commander');
 var fs = require('fs');
-var map = require('map-stream');
-var vfs = require( 'vinyl-fs' );
-var ftp = require( 'vinyl-ftp' );
+var Sftp = require('sftp-uploads');
 
 program
   .version(require('../package.json').version)
   .usage('[project name] [options] ')
-  .option('-C, --config <str>', 'specify your config file')
+  .option('-C, --config <str>', 'specify your config file, default is deploy.json')
   .parse(process.argv);
 
 var deploy_env = program.args[0];
@@ -20,21 +18,26 @@ var log = function(file, cb) {
 };
 fs.readFile(config, function(err,data) {
     if (!err) {
-        // logic here
-        var config = JSON.parse(data.toString('utf-8'))[deploy_env]
-        if (config) {   
-            var conn = new ftp(config);
-            vfs.src(config.workspace, { buffer: false } )
-                .pipe(map(log)) // print the effected files
-                .pipe( conn.dest(config.dest, {
-                    sourcemaps: config.sourcemaps,
-                }))
-            console.log('deploy success!');
+         // logic here
+        var config = JSON.parse(data.toString('utf-8'))[deploy_env];
+        if (config) {
+             sftp = new Sftp(config);
+             sftp.on('error', function(err){
+                throw err;
+             })
+             .on('uploading', function(pgs){
+                console.log('Uploading', pgs.file);
+                console.log(pgs.percent+'% completed');
+             })
+             .on('completed', function(){
+                console.log('Upload Completed');
+             })
+             .upload();
         } else {
-            console.error('deploy_env not found');
+            console.error('deploy_env', deploy_env, 'is not found');
         }
     } else {
-        console.error('config file not found');
+        console.error('config file', config, 'is not found');
     }
 });
 // print uncaughtException for debug
